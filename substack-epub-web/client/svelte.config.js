3c7fbb3e586422272125c0f2bdd377b2340b1cb1
JollyGrin <lol@dean.lol>
2025-04-01 00:09:59 +0200

    adapt

--- substack-epub-web/client/svelte.config.js
+++ substack-epub-web/client/svelte.config.js
@@ -3,7 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		// Required for SPA mode
+		paths: {
+			base: ''
+		}
+	}
 };
 
 export default config;
